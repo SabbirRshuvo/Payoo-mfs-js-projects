448c5eb960c3a8370f387470459412e4c4e6f37a
sabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-12-01 20:10:41 +0600

    some pin or amount set done

--- JS/home.js
+++ JS/home.js
@@ -1,1 +1,12 @@
-console.log("home added");
+// add money btn
+document
+  .getElementById("add-money-btn")
+  .addEventListener("click", function (event) {
+    event.preventDefault();
+    const amountInput = document.getElementById("amount-input");
+    console.log(amountInput.value);
+
+    // amount pin
+    const amountPin = document.getElementById("amount-pin");
+    console.log(amountPin.value);
+  });
