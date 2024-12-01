7aec65225f38d6de682239ad7a25842678fc3e21
sabbirRshuvo <sabbirhshuvo11@gmail.com>
2024-12-01 18:55:55 +0600

    second commit

--- index.js
+++ index.js
@@ -1,11 +1,4 @@
-document
-  .getElementById("button-login")
-  .addEventListener("click", function (event) {
-    event.preventDefault();
-    console.log("button clicked");
-
-    // step two
-
-    const phoneNumber = document.getElementById("phone-number");
-    console.log(phoneNumber);
-  });
+document.getElementById("button-login").addEventListener("click", function () {
+  const phoneNumber = document.getElementById("phone-number");
+  console.log(phoneNumber.value);
+});
